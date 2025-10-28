bb0439de459e4fcefb7be9c68c9c9e7725f88a68
Mohamed Talal Abdelmoamen <mohamedtalal881@gmail.com>
2025-10-28 15:12:50 +0300

    03-trpc-setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,25 @@
-import Image from "next/image";
-import prisma from "@/lib/db";
-import { Button } from "@/components/ui/button";
-import { json } from "stream/consumers";
+import { getQueryClient, trpc } from "@/trpc/server";
+import { caller } from "@/trpc/server";
+import { Client } from "./client";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { Suspense } from "react";
 //to find rows that i made in table user
-const page= async()=> {
-  const users = await prisma.user.findMany();
-  return (
 
+const page= async()=> {
+  const queryClient =getQueryClient();
+  void queryClient.prefetchQuery(trpc.getUsers.queryOptions());
+  return (
+// lma agy a refresh byhsl loading bdl error
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-    {JSON.stringify(users)}
+
+   <HydrationBoundary state={dehydrate(queryClient)}>
+     <Suspense fallback = {<p>loading...</p>}> 
+     
+      <Client />
+     </Suspense>
+    
+   </HydrationBoundary>
+  
     </div>
   );
 };
---
+++ src/trpc/server.tsx
@@ -0,0 +1,23 @@
+
+import 'server-only'; // <-- ensure this file cannot be imported from the client for security reasons
+import { createTRPCOptionsProxy } from '@trpc/tanstack-react-query';
+import { cache } from 'react';
+import { createTRPCContext } from './init';
+import { makeQueryClient } from '@/trpc/query-client';
+
+
+import { appRouter } from './routers/_app';
+// IMPORTANT: Create a stable getter for the query client that
+//            will return the same client during the same request.
+export const getQueryClient = cache(makeQueryClient);
+export const trpc = createTRPCOptionsProxy({
+  ctx: createTRPCContext,
+  router: appRouter,
+  queryClient: getQueryClient,
+});
+
+// ...
+import { createCallerFactory } from './init';
+
+export const caller = createCallerFactory(appRouter)(createTRPCContext);
+
