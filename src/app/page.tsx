e640db11350c79a0dc2709ef21b89a8d2ff57b74
Mohamed Talal Abdelmoamen <mohamedtalal881@gmail.com>
2025-10-29 23:19:36 +0300

    08-again

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,37 @@
+
+"use client"
 import { requireAuth } from "@/lib/auth-utils";
 
 import {caller } from "@/trpc/server"
 import { LogoutButton } from "./logout";
-const Page = async() => {
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useTRPC } from "@/trpc/client";
+import { Button } from "@/components/ui/button";
+import { createECDH } from "crypto";
+import { toast } from "sonner";
+const Page = () => {
 
-await requireAuth();
 
- const data= await caller.getUsers();
+const trpc= useTRPC();
+const queryClient= useQueryClient();
+ const {data}= useQuery(trpc.getWorkflows.queryOptions());
+const testAi=useMutation(trpc.testAi.mutationOptions({
+  onSuccess : () =>{
+    toast.success("AI Job queued");
+  },
+  onError: () => {
+    toast.error("something went wrong");
+  }
+}));
+
+ const create = useMutation(trpc.createWorkflow.mutationOptions({
+onSuccess:()=>{
+ toast.success("job queued");
+
+}
+
+ }));
+
 
   return (
     <div className="min-h-screen min-w-screen flex items-center justify-center flex-col gap-y-6">
@@ -15,6 +40,8 @@ await requireAuth();
       <div>
         {JSON.stringify(data,null,2)}
       </div>
+      <Button  disabled={testAi.isPending} onClick={()=>testAi.mutate()} >Test AI</Button>
+      <Button disabled={create.isPending } onClick={()=>create.mutate()}>Create workflow </Button>
       <LogoutButton/>
     </div>
   );
