2bb39ace3c73fe547b12e090d3fc3c7049897a34
Mohamed Talal Abdelmoamen <mohamedtalal881@gmail.com>
2025-11-01 19:14:48 +0200

    16-node-selector

--- src/components/initial-node.tsx
+++ src/components/initial-node.tsx
@@ -3,16 +3,20 @@
 
 import type{NodeProps} from "@xyflow/react";
 import { PlusIcon } from "lucide-react";
-import{memo} from "react";
+import{memo , useState} from "react";
 import { PlaceholderNode } from "./react-flow/placeholder-node";
 import { WorkflowNode } from "./workflow-node";
+import { NodeSelector } from "./node-selector";
 
 export const InitialNode = memo ((props : NodeProps)=>{
-    return (
+  const [selectorOpen,setSelectorOpen] = useState(false);
+   
+   return (
+<NodeSelector open={selectorOpen} onOpenChange={setSelectorOpen}>
 <WorkflowNode showToolbar={false}>
      <PlaceholderNode
      {...props} 
-     onClick={()=>{}}
+     onClick={()=>setSelectorOpen(true)}
      >
       <div className="cursor-pointer flex items-center justify-center">
         <PlusIcon className="size-4"/>  
@@ -20,6 +24,7 @@ export const InitialNode = memo ((props : NodeProps)=>{
 
      </PlaceholderNode>
 </WorkflowNode>
+</NodeSelector>
     )
 });
 
