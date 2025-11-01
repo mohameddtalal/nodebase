848ce88b0e737a59ab60822684a0a3c765622280
Mohamed Talal Abdelmoamen <mohamedtalal881@gmail.com>
2025-11-01 16:15:21 +0200

    1-editor

--- src/features/editor/components/editor.tsx
+++ src/features/editor/components/editor.tsx
@@ -1,8 +1,28 @@
 "use client";
 
-
+import {useState,useCallback} from 'react';
+import{ReactFlow,
+    applyNodeChanges,
+    applyEdgeChanges,
+   type Node,
+    addEdge,
+    type Edge,
+   type NodeChange,
+   type EdgeChange,
+   type Connection,
+   Background,
+   Controls,
+   MiniMap,
+   Panel,
+} from '@xyflow/react';
 import { ErrorView, LoadingView } from "@/components/entity-components";
 import { useSuspenseWorkflow } from "@/features/workflows/hooks/use-workflows";
+import '@xyflow/react/dist/style.css';
+import { nodeComponents } from '@/config/node-components';
+
+import { AddNodeButton } from './add-node-button';
+
+
 
 export const EditorLoading =() =>{
     return <LoadingView message="Loading editor ..."/>;
@@ -15,12 +35,48 @@ return <ErrorView message="Error loading editor"/>;
 
 
 
+
 export const Editor =({workflowId}:{workflowId:string}) =>{
 const {data: workflow } = useSuspenseWorkflow(workflowId);
-return (
-<p>
-{JSON.stringify (workflow,null,2)}
 
-</p>
+const [nodes, setNodes] = useState<Node[]>(workflow.nodes);
+const [edges, setEdges] = useState<Edge[]>(workflow.edges);
+
+
+ const onNodesChange = useCallback( //snapshot current valye of node
+    (changes : NodeChange[]) => setNodes((nodesSnapshot) => applyNodeChanges(changes, nodesSnapshot)),
+    [],
+  );
+  const onEdgesChange = useCallback(
+    (changes : EdgeChange[]) => setEdges((edgesSnapshot) => applyEdgeChanges(changes, edgesSnapshot)),
+    [],
+  );
+  const onConnect = useCallback(
+    (params: Connection) => setEdges((edgesSnapshot) => addEdge(params, edgesSnapshot)),
+    [],
+  );
+
+return ( //controls feha zoom out w zoom in w keda
+<div className='size-full'>
+    <ReactFlow
+    nodes={nodes}
+    edges={edges}
+    onNodesChange={onNodesChange}
+    onEdgesChange={onEdgesChange}
+    onConnect={onConnect}
+    nodeTypes={nodeComponents}
+        fitView
+        proOptions={{    //to hide logo bta3hom taht
+            hideAttribution :true,
+        }}
+>
+<Background/>
+<Controls/> 
+<MiniMap/>
+<Panel position = "top-right">
+  <AddNodeButton/>
+</Panel>
+</ReactFlow>
+</div>
 );
 }; 
