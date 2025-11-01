4da2568cdf5590098fb18b50d4bf7b73f884c157
Mohamed Talal Abdelmoamen <mohamedtalal881@gmail.com>
2025-11-01 13:18:38 +0200

    1-workflow-page

--- src/features/workflows/server/routers.ts
+++ src/features/workflows/server/routers.ts
@@ -33,8 +33,9 @@ return prisma.workflow.update({
     data:{name:input.name},
 });
 }),
+
 getOne: protectedProcedure.input(z.object({id:z.string()})).query(({ctx,input})=>{
-    return prisma.workflow.findUnique({
+    return prisma.workflow.findUniqueOrThrow({
         where:{
             id:input.id, userId:ctx.auth.user.id
 
